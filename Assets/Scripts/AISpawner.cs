404b00a7b0442ecf294c34ea50875081b375f446
Micnasr <micraidz@gmail.com>
2023-07-17 15:46:24 -0400

    Basic Respawning and Random Prefab in spawners

--- Assets/Scripts/AISpawner.cs
+++ Assets/Scripts/AISpawner.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 
 public class AISpawner : MonoBehaviour
 {
-    public GameObject fishPrefab;
+    public GameObject[] fishPrefabs;
     public List<Transform> Waypoints = new List<Transform>();
 
-    // Start is called before the first frame update
-    void Start()
+    private GameObject currentFish;
+    private bool isRespawning = false; // Flag to indicate if a fish is currently being respawned
+
+    public float respawnDelay = 10f;
+
+    private void Start()
     {
         GetWaypoints();
         SpawnFish();
+    }
+
+    private void Update()
+    {
+        // Check if the current fish is dead and not already respawning
+        if (currentFish == null && !isRespawning)
+        {
+            StartCoroutine(RespawnFish());
+        }
     }
 
     void GetWaypoints()
@@ -20,7 +32,7 @@ public class AISpawner : MonoBehaviour
         Transform[] wpList = this.transform.GetComponentsInChildren<Transform>();
         for (int i = 0; i < wpList.Length; i++)
         {
-            if (wpList[i].tag == "waypoint") 
+            if (wpList[i].tag == "waypoint")
             {
                 Waypoints.Add(wpList[i]);
             }
@@ -29,14 +41,31 @@ public class AISpawner : MonoBehaviour
 
     void SpawnFish()
     {
-        GameObject fish = Instantiate(fishPrefab, transform.position, transform.rotation);
+        int randomIndex = Random.Range(0, fishPrefabs.Length);
+        currentFish = Instantiate(fishPrefabs[randomIndex], transform.position, transform.rotation);
 
-        EnemyPatrol fishPatrol = fish.GetComponent<EnemyPatrol>();
-        LandEnemyPatrol landFishPatrol = fish.GetComponent<LandEnemyPatrol>();
-        
-        if (fishPatrol != null )
+        // Set up fish patrol waypoints or other logic
+        EnemyPatrol fishPatrol = currentFish.GetComponent<EnemyPatrol>();
+        LandEnemyPatrol landFishPatrol = currentFish.GetComponent<LandEnemyPatrol>();
+
+        if (fishPatrol != null)
             fishPatrol.waypoints = Waypoints;
         else
             landFishPatrol.waypoints = Waypoints;
+    }
+
+    private IEnumerator RespawnFish()
+    {
+        // Set the flag to prevent multiple respawns
+        isRespawning = true;
+
+        // Wait for the respawn delay
+        yield return new WaitForSeconds(respawnDelay);
+
+        // Call SpawnFish() again to respawn the fish
+        SpawnFish();
+
+        // Reset the flag
+        isRespawning = false;
     }
 }
