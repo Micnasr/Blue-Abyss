1d62263b6ffe8b54dcceb1476ae48de147348d38
Micnasr <micraidz@gmail.com>
2023-07-17 17:32:46 -0400

    Smarter Spawning System

--- Assets/Scripts/AISpawner.cs
+++ Assets/Scripts/AISpawner.cs
@@ -11,9 +11,18 @@ public class AISpawner : MonoBehaviour
     private bool isRespawning = false; // Flag to indicate if a fish is currently being respawned
 
     public float respawnDelay = 10f;
+    public float spawnDistanceThreshold = 20f;
+
+    private Camera playerCam;
+    private GameObject player;
+
 
     private void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+            playerCam = player.GetComponentInChildren<Camera>();
+
         GetWaypoints();
         SpawnFish();
     }
@@ -62,10 +71,51 @@ public class AISpawner : MonoBehaviour
         // Wait for the respawn delay
         yield return new WaitForSeconds(respawnDelay);
 
-        // Call SpawnFish() again to respawn the fish
-        SpawnFish();
+        // Only Spawn If Not Looking Directly Towards and x Away
+        if (!IsPlayerLookingAtSpawnPoint(this.gameObject) && !PlayerInRange(spawnDistanceThreshold))
+            SpawnFish();
 
-        // Reset the flag
         isRespawning = false;
     }
+
+    private bool IsPlayerLookingAtSpawnPoint(GameObject spawnPoint)
+    {
+        // Get the player's forward direction
+        Vector3 playerForward = playerCam.transform.forward;
+
+        // Calculate the vector from the player to the spawn point
+        Vector3 playerToSpawnPoint = spawnPoint.transform.position - playerCam.transform.position;
+
+        // Normalize the vectors
+        playerForward.Normalize();
+        playerToSpawnPoint.Normalize();
+
+        // Calculate the dot product between the vectors
+        float dotProduct = Vector3.Dot(playerForward, playerToSpawnPoint);
+
+        // Define a threshold value (adjust as needed)
+        float angleThreshold = 0.8f;
+
+        // Check if the dot product is greater than the threshold
+        if (dotProduct > angleThreshold)
+        {
+            // Player is looking towards the spawn point
+            return true;
+        }
+
+        // Player is not looking towards the spawn point
+        return false;
+    }
+
+    public bool PlayerInRange(float range)
+    {
+        if (player == null)
+        {
+            return false;
+        }
+
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        return distance <= range;
+    }
+
 }
