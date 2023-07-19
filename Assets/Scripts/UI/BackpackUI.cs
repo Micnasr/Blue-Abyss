0e5534b9ba4c35d8b884108f3f5f1e1baaddf52b
Micnasr <micraidz@gmail.com>
2023-07-18 20:46:13 -0400

    Basic Icon Art + Rescaling backdrop with item count

--- Assets/Scripts/UI/BackpackUI.cs
+++ Assets/Scripts/UI/BackpackUI.cs
@@ -39,17 +39,54 @@ public class BackpackUI : MonoBehaviour
             {
                 itemsUnlocked.Add(1);
             }
+            else if (strArray[i] == "Light")
+            {
+                itemsUnlocked.Add(2);
+            }
+            else if (strArray[i] == "Jacket")
+            {
+                itemsUnlocked.Add(3);
+            }
         }
 
     }
 
     private void GenerateUI()
     {
+        float posX = 0f;
+        float width = 0f;
+
+        switch (itemsUnlocked.Count)
+        {
+            case 1:
+                posX = -11.36689f;
+                width = 72.981f;
+                break;
+            case 2:
+                posX = 4.6226f;
+                width = 104.96f;
+                break;
+            case 3:
+                posX = 21.2f;
+                width = 138.11f;
+                break;
+            // Add more cases for other counts as needed
+            default:
+                posX = 37.5f;
+                width = 170.82f;
+                break;
+        }
+
+        RectTransform rectTransform = backgroundPanel.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(posX, rectTransform.anchoredPosition.y);
+        rectTransform.sizeDelta = new Vector2(width, rectTransform.sizeDelta.y);
+
         for (int i = 0; i < itemsUnlocked.Count; i++)
         {
             GameObject instantiatedPrefab = Instantiate(prefabs[itemsUnlocked[i]], positions[i].position, positions[i].rotation);
             instantiatedPrefab.transform.SetParent(backgroundPanel.transform);
         }
+
     }
 
 
