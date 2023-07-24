05b2afa4fe82ce3a41ae54f47a254c8f14122975
Micnasr <micraidz@gmail.com>
2023-07-24 00:28:09 -0400

    Very Basic Vehicle Controller
    This took so many hours :sob: Still need to add a plane to stop waves from going inside & adjust settings to make it more nice to drive

--- Assets/Scripts/Vehicles/VehicleController.cs
+++ Assets/Scripts/Vehicles/VehicleController.cs
@@ -1,18 +1,143 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class VehicleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform driverSeat;
+    public Transform exitLocation;
+
+    public KeyCode interactKey = KeyCode.E;
+
+    [Header("Vehicle Stats")]
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 100f;
+
+    private bool isDriving = false;
+
+    private Transform player;
+    private Rigidbody playerRigidbody;
+    private Rigidbody boatRigidbody;
+    private PlayerMovement playerMovement;
+    private PlayerShoot playerShoot;
+    private PlayerCam playerCam;
+    private WeaponSway weaponSway;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerRigidbody = player.GetComponent<Rigidbody>();
+
+        playerMovement = player.gameObject.GetComponent<PlayerMovement>();
+        playerShoot = player.gameObject.GetComponent<PlayerShoot>();
+        playerCam = player.gameObject.GetComponentInChildren<PlayerCam>();
+        weaponSway = player.gameObject.GetComponentInChildren<WeaponSway>();
+
+        boatRigidbody = GetComponent<Rigidbody>();
+        if (boatRigidbody == null)
+        {
+            boatRigidbody = gameObject.AddComponent<Rigidbody>();
+            boatRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (isDriving)
+        {
+            HandleDriving();
+        }
+    }
+
+    private void Update()
+    {
+        if (!isDriving)
+        {
+            HandleLookingAtVehicle();
+        }
+        else
+        {
+            // Check if the player presses the interact key (E) to exit the vehicle
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                ExitVehicle();
+                Debug.Log("Exiting Vehicle");
+                return;
+            }
+        }
+    }
+
+    private void HandleLookingAtVehicle()
+    {
+        // Check if the player is looking at the vehicle and presses the interact key (E)
+        if (Input.GetKeyDown(interactKey) && CanEnterVehicle())
+        {
+            Debug.Log("Entering Vehicle");
+            EnterVehicle();
+        }
+    }
+
+    private bool CanEnterVehicle()
+    {
+        RaycastHit hit;
+        float maxInteractDistance = 5f;
+
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, maxInteractDistance))
+        {
+            // Check if the raycast hits the vehicle's collider
+            if (hit.collider.gameObject.transform.parent.gameObject == gameObject)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void EnterVehicle()
+    {
+        // Disable Player Scripts
+        playerMovement.enabled = false;
+        playerShoot.enabled = false;
+        weaponSway.enabled = false;
+
+        playerRigidbody.velocity = Vector3.zero;
+        playerRigidbody.isKinematic = true;
+
+        // Move the player to the driver seat position and rotate them to face the boat's forward direction
+        player.position = driverSeat.position;
+        player.rotation = driverSeat.rotation;
+        player.SetParent(driverSeat);
+
+        isDriving = true;
+    }
+
+    private void HandleDriving()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        float rotationInput = Input.GetAxis("Horizontal");
+
+        Vector3 rotationTorque = Vector3.up * rotationInput * rotationSpeed;
+        boatRigidbody.AddTorque(rotationTorque, ForceMode.Force);
+
+        // Move the boat forward or backward
+        Vector3 forwardForce = transform.forward * forwardInput * moveSpeed;
+        boatRigidbody.AddForce(forwardForce, ForceMode.Force);
+    }
+
+
+    private void ExitVehicle()
+    {
+        // Enable player scripts
+        playerMovement.enabled = true;
+        playerShoot.enabled = true;
+        weaponSway.enabled = true;
+        playerRigidbody.isKinematic = false;
+
+        // Teleport the player to the exit location and reset their rotation
+        player.position = exitLocation.position;
+        player.rotation = exitLocation.rotation;
+        player.SetParent(null);
+
+        isDriving = false;
     }
 }
