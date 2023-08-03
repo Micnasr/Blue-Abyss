91d9feabc956377d6e24f4554424631a125f8f49
Micnasr <micraidz@gmail.com>
2023-08-02 20:13:34 -0400

    Very Simple Dialogue and NPC Interaction Logic

---
+++ Assets/Scripts/Quest/NPCInteract.cs
@@ -0,0 +1,140 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteract : MonoBehaviour
+{
+    private Transform player;
+    private PlayerMovement playerMovement;
+    private PlayerShoot playerShoot;
+    private PlayerCam playerCam;
+    private WeaponSway weaponSway;
+
+    private Dialogue dialogueManager;
+
+    public float interactionDistance = 5f;
+    public float rotationSpeed = 5f;
+
+    public KeyCode interactKey = KeyCode.E;
+
+    // For The Hover Text
+    public string interactMessage;
+    private bool interactedOn = false;
+
+    private bool talkingWithPlayer = false;
+
+    [Header("Text Lines")]
+    public string[] lines;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        playerMovement = player.gameObject.GetComponent<PlayerMovement>();
+        playerShoot = player.gameObject.GetComponent<PlayerShoot>();
+        playerCam = player.gameObject.GetComponentInChildren<PlayerCam>();
+        weaponSway = player.gameObject.GetComponentInChildren<WeaponSway>();
+
+        dialogueManager = FindAnyObjectByType<Dialogue>();
+    }
+
+    private void Update()
+    {
+        // Check if the player is near the NPC
+        if (Vector3.Distance(playerCam.transform.position, transform.position) <= interactionDistance)
+        {
+            RotateNPC();
+
+            if (Input.GetKeyDown(interactKey) && playerMovement.grounded && LookingAtNPC())
+            {
+                if (!talkingWithPlayer)
+                {
+                    InteractWith();
+                }
+            }
+        }
+        
+        // Handle Interact UI Render
+        if (!interactedOn && (Vector3.Distance(playerCam.transform.position, transform.position) <= interactionDistance) && LookingAtNPC() && !talkingWithPlayer && playerMovement.grounded)
+        {
+            FindAnyObjectByType<InteractUI>().InteractWith(interactMessage);
+            interactedOn = true;
+
+        }
+        else if (interactedOn && (!playerMovement.grounded || talkingWithPlayer || Vector3.Distance(playerCam.transform.position, transform.position) >= interactionDistance || !LookingAtNPC()))
+        {
+            FindAnyObjectByType<InteractUI>().InteractStop();
+            interactedOn = false;
+        }
+    }
+
+    private bool LookingAtNPC()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, 5f))
+        {
+            if (hit.collider != null && hit.collider.gameObject != null && hit.collider.gameObject.transform.parent != null)
+            {
+                if (hit.collider.gameObject == gameObject)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void CloseDialogue()
+    {
+        talkingWithPlayer = false;
+
+        dialogueManager.CloseDialogue();
+        
+        playerMovement.enabled = true;
+        playerShoot.enabled = true;
+        playerCam.enabled = true;
+        weaponSway.enabled = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void InteractWith()
+    {
+        talkingWithPlayer = true;
+
+        dialogueManager.StartDialogue(lines, this);
+
+        playerMovement.enabled = false;
+        playerShoot.enabled = false;
+        playerCam.enabled = false;
+        weaponSway.enabled = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    private void RotateNPC()
+    {
+        // Calculate the direction from NPC to player
+        Vector3 directionToPlayer = player.position - transform.position;
+        directionToPlayer.y = 0f;
+
+        // Smoothly rotate the NPC towards the player
+        Quaternion targetRotation = Quaternion.LookRotation(directionToPlayer);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    public void AcceptQuest()
+    {
+        CloseDialogue();
+    }
+
+    public void DeclineQuest()
+    {
+        CloseDialogue();
+    }
+}
---
+++ Assets/Scripts/UI/Dialogue.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    private string[] lines;
+    public float textSpeed;
+
+    // Quest Related
+    public GameObject yesButton;
+    public GameObject noButton;
+
+    private NPCInteract currentNPC;
+
+    private int index;
+
+    void Start()
+    {
+        gameObject.SetActive(false);
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
+
+        textComponent.text = string.Empty;
+    }
+
+    void Update()
+    {
+        // Fast Forward Dialogue
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            } 
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+    }
+
+    public void StartDialogue(string[] texts, NPCInteract npc)
+    {
+        currentNPC = npc;
+        lines = texts;
+        index = 0;
+        gameObject.SetActive(true);
+        textComponent.text = string.Empty;
+
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach(char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index >= lines.Length - 1)
+            return;
+
+        if (index == lines.Length - 2)
+        {
+            // Show Quest Accept/Deny Buttons
+            yesButton.SetActive(true);
+            noButton.SetActive(true);
+        }
+
+        index++;
+        textComponent.text = string.Empty;
+        StartCoroutine(TypeLine());
+    }
+
+    public void CloseDialogue()
+    {
+        gameObject.SetActive(false);
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
+    }
+
+    public void AcceptQuest()
+    {
+        if (currentNPC == null)
+            Debug.LogError("CurrentNPC Null");
+
+        currentNPC.AcceptQuest();
+    }
+
+    public void DeclineQuest()
+    {
+        if (currentNPC == null)
+            Debug.LogError("CurrentNPC Null");
+
+        currentNPC.DeclineQuest();
+    }
+}
