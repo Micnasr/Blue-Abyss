1a001394a3e66504d7952b7e4596ae3a8efe59c1
Micnasr <micraidz@gmail.com>
2023-08-09 00:05:33 -0400

    NPCS wont talk / give quests if you dont own a jacket

--- Assets/Scripts/Player/WinterBiome.cs
+++ Assets/Scripts/Player/WinterBiome.cs
@@ -13,6 +13,8 @@ public class WinterBiome : MonoBehaviour
     private GameObject currentSnowParticle;
     private BackpackUI backpackUI;
 
+    public GameObject[] winterNPCS;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -24,10 +26,13 @@ public class WinterBiome : MonoBehaviour
 
     void Update()
     {
+        // Check if Player Owns Jacket
+        bool hasJacket = backpackUI.itemsUnlockedStr.Contains("Jacket");
+
         if (playerMovement.GetSurfaceType() == "ice-surface")
         {
             // If Player Owns Jacket, Do Not Take Damage
-            if (!backpackUI.itemsUnlockedStr.Contains("Jacket") && healthManager != null)
+            if (!hasJacket && healthManager != null)
             {
                 float damage = freezeDamage * Time.deltaTime;
                 healthManager.TakeDamage(damage, 0f);
@@ -49,6 +54,15 @@ public class WinterBiome : MonoBehaviour
                 Destroy(currentSnowParticle);
                 currentSnowParticle = null;
             }
+        }
+
+        // Enable / Disable Npcs Depending On If Player Owns Jacket
+        foreach (GameObject NPC in winterNPCS)
+        {
+            NPCInteract npcInteract = NPC.GetComponent<NPCInteract>();
+
+            if (npcInteract.enabled == !hasJacket)
+                npcInteract.enabled = hasJacket;
         }
     }
 }
