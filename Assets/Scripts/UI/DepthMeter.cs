8710b8fe9f43c0dcd195764cc81dbba96e3d8ee4
Micnasr <micraidz@gmail.com>
2023-08-10 21:47:29 -0400

    DepthMeter

---
+++ Assets/Scripts/UI/DepthMeter.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class DepthMeter : MonoBehaviour
+{
+    private Transform player;
+    private TextMeshProUGUI depthText;
+
+    private float initialYPosition = -4.0f;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        depthText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        // Calculate the depth based on the player's Y position
+        float depth = Mathf.Abs((player.position.y - initialYPosition) * -2.0f);
+
+        // Update the depth text
+        depthText.text = depth.ToString("F1") + "m";
+    }
+}
